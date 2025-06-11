fcbe17ff84d4b63fd2fe753d6d68430a704dd6ec
EimantasJaskonis <eimantasjaskonis@gmail.com>
2025-06-11 17:35:45 +0300

    userRoutes.js edited

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,17 @@
 import express from 'express';
-import { registerUser, loginUser } from '../controllers/userController.js';
+import { 
+    registerUser,
+    loginUser,
+    updateProfile,
+    updatePassword
+} from '../controllers/userController.js';
+import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.patch('/user/profile', authMiddleware, updateProfile);
+router.patch('/user/password', authMiddleware, updatePassword);
 
 export default router;
