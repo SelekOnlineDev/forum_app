39129988bdbc61306707d07d06d0a99fece3da90
EimantasJaskonis <eimantasjaskonis@gmail.com>
2025-06-13 20:10:11 +0300

    userController.js edited

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,9 +10,10 @@ export const registerUser = async (req, res) => {
     const { name, email, password } = req.body;
 
     const existing = await users.findOne({ email });
-    if (existing) return res.status(400).json({ message: 'User already exists' });
-    const newuser = await users.insertOne(newUser);
-    if (newuser) return res.status(400).json({ message: 'User already exists' });
+    if (existing) {
+      console.log('User already exists:', email);
+    return res.status(400).json({ message: 'User already exists' });
+    };
 
     const hashed = await bcrypt.hash(password, 10);
     const newUser = {
@@ -38,8 +39,6 @@ export const registerUser = async (req, res) => {
       }
     });
 
-    await users.insertOne(newUser);
-    res.status(201).json({ message: 'User registered', id: newUser._id });
   } catch (err) {
     console.error('Register error:', err);
     res.status(500).json({ message: 'Server error' });
@@ -51,11 +50,11 @@ export const loginUser = async (req, res) => {
     const db = await getDb();
     const users = db.collection('users');
     const { email, password } = req.body;
-    console.log('Login attempt for:', req.body.email);
+    console.log('Login attempt for:', email);
 
     const user = await users.findOne({ email: email });
     console.log('User found:', user); // Ar vartotojas rastas?
-    if (user) {
+    if (!user) {
      console.log(`User not found: ${email}`);
      return res.status(404).json({ message: 'User not found' });
     }
@@ -63,12 +62,16 @@ export const loginUser = async (req, res) => {
     console.log('Stored hash:', user.password);
     const match = await bcrypt.compare(password, user.password);
     console.log('Password match:', match); // Ar slaptažodis teisingas?
-    if (!match) return res.status(401).json({ message: 'Invalid credentials' });
+    if (!match) {
+     console.log('Invalid password for user:', email);
+     return res.status(401).json({ message: 'Invalid credentials' });
+    }
 
     
     const token = jwt.sign ({ id: user._id, name: user.name, email: user.email }, 
     process.env.JWT_SECRET, { expiresIn: '7d' });
-    res.json({ token, user: { id: user._id, name: user.name, email: user.email } });
+    console.log('Login successful for user:', email);
+    res.status(201).json({ token, user: { id: user._id, name: user.name, email: user.email } });
   } catch (err) {
     console.error('Login error:', err);
     res.status(500).json({ message: 'Server error' });
